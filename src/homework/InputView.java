725b7dcb93b30993d269a91286d38f8275bb2647
wkw1 <897381813@qq.com>
2017-12-23 14:18:31 +0800

    删除不必要的信息

--- src/homework/InputView.java
+++ src/homework/InputView.java
@@ -20,22 +20,6 @@ public class InputView {
 	private JTextField textField;
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					InputView window = new InputView();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
 	 * Create the application.
 	 */
 	public InputView() {
